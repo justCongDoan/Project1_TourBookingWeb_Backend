121356502d179e7fe88eeb267e05fec6514a09a4
justCongDoan <congdoan2002tb2@gmail.com>
2023-04-05 08:04:22 +0700

    1st update - backend

---
+++ backend/controllers/bookingController.js
@@ -0,0 +1,60 @@
+import Booking from '../models/Booking.js';
+
+// create new booking
+export const createBooking = async(req, res) => {
+    const newBooking = new Booking(req.body);
+    try {
+        const savedBooking = await newBooking.save();
+
+        res.status(200).json({
+            success: true,
+            message: 'Tour booked successfully',
+            data: savedBooking
+        });
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: 'Internal server error!'
+        });
+    }
+};
+
+// get single booking
+export const getBooking = async(req, res) => {
+    const id = req.params.id;
+
+    try {
+        const book = await Booking.findById(id);
+
+        res.status(200).json({
+            success: true,
+            message: "Get booking successfully!",
+            data: book
+        });
+    } catch (err) {
+        res.status(404).json({
+            success: true,
+            message: "Tour booking not found!"
+        });
+    }
+};
+
+// get all booking
+export const getAllBooking = async(req, res) => {
+    const id = req.params.id;
+
+    try {
+        const book = await Booking.find();
+
+        res.status(200).json({
+            success: true,
+            message: "All tour bookings are here!",
+            data: book
+        });
+    } catch (err) {
+        res.status(500).json({
+            success: true,
+            message: "internal server error!"
+        });
+    }
+};
